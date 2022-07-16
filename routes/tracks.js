1a4838e94a8681d7234a23ca257317a3103a41f2
oscar castro <oscar-zurdo@hotmail.es>
2022-07-16 11:45:48 -0500

    cambios 16-07-2022

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const { route } = require("express/lib/application");
-const { getItems, createItem } = require("../controllers/track");
+const customHeader = require("../middleware/customHeaders")
+const {validatorCreateItem} = require("../validators/track");
+const { getItems, getItem, createItem } = require("../controllers/track");
 const router = express.Router();
 
 //vamos a declarar nuestras ruta para el modelo tracks comprendiendo los metodo GET, POST, DELETE, PUT
 
 router.get("/", getItems);
-router.post("/", createItem)
+router.post("/",validatorCreateItem, customHeader, createItem);
 //router.get("/:id", getItem);
 
 module.exports = router
